a94d1d2eaef23ea37f219612ca82c5a1a8ef487a
l-0-w <typeint@proton.me>
2026-03-27 12:58:57 -0300

    criando classe nativa

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,72 @@
 #include "RED4ext/Api/v1/Runtime.hpp"
 #include "RED4ext/Api/v1/SemVer.hpp"
 #include "RED4ext/Api/v1/Version.hpp"
+#include "RED4ext/CNamePool.hpp"
+#include "RED4ext/Common.hpp"
+#include "RED4ext/RTTISystem.hpp"
+#include "RED4ext/RTTITypes.hpp"
+#include "RED4ext/Scripting/Functions.hpp"
+#include "RED4ext/Scripting/IScriptable.hpp"
+#include "RED4ext/Scripting/Stack.hpp"
 #include <RED4ext/RED4ext.hpp>
+#include <cstdint>
+
+struct MyCustomClass : RED4ext::IScriptable
+{
+    RED4ext::CClass* GetNativeType();
+};
+
+RED4ext::TTypedClass<MyCustomClass> cls("MyCustomClass");
+
+RED4ext::CClass* MyCustomClass::GetNativeType() 
+{
+    return &cls;
+}
+
+void GetNumber(RED4ext::IScriptable* aContext, RED4ext::CStackFrame* aFrame, float* aOut, int64_t a4)
+{
+    RED4EXT_UNUSED_PARAMETER(aContext);
+    RED4EXT_UNUSED_PARAMETER(a4);
+
+    aFrame->code++;
+    *aOut = 6.25;
+}
+
+RED4EXT_C_EXPORT void RED4EXT_CALL RegisterTypes()
+{
+    RED4ext::CNamePool::Add("MyCustomClass");
+
+    cls.flags = { .isNative = true };
+    RED4ext::CRTTISystem::Get()->RegisterType(&cls);
+}
+
+RED4EXT_C_EXPORT void RED4EXT_CALL PostRegisterTypes()
+{
+    auto rtti = RED4ext::CRTTISystem::Get();
+    auto scriptable = rtti->GetClass("IScriptable");
+    cls.parent = scriptable;
+
+    RED4ext::CBaseFunction::Flags flags =  {.isNative = true};
+    auto getNumber = RED4ext::CClassFunction::Create(&cls, "GetNumber", "GetNumber", &GetNumber);
+    getNumber->flags = flags;
+    getNumber->SetReturnType("Float");
+    cls.RegisterFunction(getNumber);
+}
 
 RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::v1::PluginHandle aHandle, RED4ext::v1::EMainReason aReason, const RED4ext::v1::Sdk* aSdk)
 {
+    RED4EXT_UNUSED_PARAMETER(aHandle);
+    
     switch (aReason)
     {
     case RED4ext::v1::EMainReason::Load:
     {
-        aSdk->logger->Trace(aHandle, "Olá Mundo!");
+        auto rtti = RED4ext::CRTTISystem::Get();
+
+        rtti->AddRegisterCallback(RegisterTypes);
+        rtti->AddPostRegisterCallback(PostRegisterTypes);
+        
+        aSdk->logger->Trace(aHandle, "Olá Mundo!");    
         break;
     }
     case RED4ext::v1::EMainReason::Unload:
